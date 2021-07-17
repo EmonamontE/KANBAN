88f896ed423b6cf57062782377382e37a998e870
emonamonte <racoonsmr@gmail.com>
2021-07-17 18:04:10 +0400

    Vuex is now connected to LocalStorage

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
 
 Vue.use(Vuex)
 
@@ -11,6 +12,8 @@ export default new Vuex.Store({
     needsReviewState: [],
     approvedState: []
   },
+
+  plugins: [createPersistedState()],
 
   mutations: {
     incrementCurrentId (state) {
